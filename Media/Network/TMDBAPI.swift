49ac9ee3b889462fee2d13b243a20b5436a89aa7
yoonwooiOS <yoonwoo4429@kakao.com>
2024-06-29 17:08:28 +0900

    🎨 refactor : EndPoint URL타입에서 String 타입으로 변경

--- Media/Network/TMDBAPI.swift
+++ Media/Network/TMDBAPI.swift
@@ -16,26 +16,28 @@ enum TMDBAPI {
     case moviePoster(query: Int)
     case genres
     case cast(query: Int)
+    
+    
     var baseURL: String {
         return "https://api.themoviedb.org/3/"
     }
     
-    var endPoint: URL {
+    var endPoint: String {
         switch self {
         case .trendingMovie:
-            return URL(string: baseURL + "trending/movie/day?")!
+            return baseURL + "trending/movie/day?"
         case .similarMovie(let movieId):
-            return URL(string: baseURL + "movie/\(movieId)/similar")!
+            return  baseURL + "movie/\(movieId)/similar"
         case .recommandMovie(let movieId):
-            return URL(string: baseURL + "movie/\(movieId)/recommendations")!
+            return baseURL + "movie/\(movieId)/recommendations"
         case .search:
-            return URL(string: baseURL + "search/movie?")!
+            return baseURL + "search/movie?"
         case .moviePoster(query: let movieId):
-            return URL(string: baseURL + "movie/\(movieId)/images")!
+            return baseURL + "movie/\(movieId)/images"
         case .genres:
-            return URL(string: baseURL + "genre/movie/list")!
+            return baseURL + "genre/movie/list"
         case .cast(query: let query):
-            return URL(string: baseURL + "movie/\(query)/credits")!
+            return baseURL + "movie/\(query)/credits"
         }
     }
     var header: HTTPHeaders {
@@ -54,17 +56,16 @@ enum TMDBAPI {
             return ["query" : movieId,
                     "language" : "ko-KR"
             ]
-        case .recommandMovie(query: let movieId):
-            return ["query" : movieId,
-                    "language" : "ko-KR"
+        case .recommandMovie(query: _):
+            return [
+                "language" : "ko-KR"
             ]
         case .search(query: let movie):
             return ["query" : movie,
                     "language" : "ko-KR"
             ]
         case .moviePoster(query: let movieId):
-            return ["query" : movieId,
-                    "language" : "ko-KR"]
+            return ["query" : movieId]
         case .genres:
             return ["language" : "ko-KR"]
         case .cast(query: let movieId):
